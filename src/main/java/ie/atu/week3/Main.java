f673216aa9164cd3fbe0f9fcd01103dddc4d2ab3
Sami Hammar <G00393324@atu.ie>
2025-10-01 17:07:59 +0100

    expand calc function Commit

--- src/main/java/ie/atu/week3/Main.java
+++ src/main/java/ie/atu/week3/Main.java
@@ -1,19 +1,22 @@
 package ie.atu.week3;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        // Create an instance of Calculator
+        Calculator calc = new Calculator();
+        System.out.println("Simple Calculator - Add Two Numbers");
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int result = calc.add(a, b);
+        System.out.println("Result: " + result);
+        sc.close();
     }
+    //test
 }
